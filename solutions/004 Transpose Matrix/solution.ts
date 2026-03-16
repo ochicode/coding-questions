3837bc36fea1eddc89b5b2b59a09d4f347e998ad
Benard <benard518@gmail.com>
2026-03-16 07:33:26 +0300

    solution004

---
+++ solutions/004 Transpose Matrix/solution.ts
@@ -0,0 +1,14 @@
+function transpose(matrix: number[][]): number[][] {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    const result: number[][] = Array.from({ length: n }, () => Array(m));
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            result[j][i] = matrix[i][j];
+        }
+    }
+
+    return result
+};
